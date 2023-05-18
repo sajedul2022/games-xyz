fe6a3b54698990a187fff60827e13f1562ae6de4
sajedul2022 <sajedul.idb.info@gmail.com>
2023-05-18 14:49:36 +0600

    first commit

---
+++ src/components/frontend/include/footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer className="footer-section">
+        <div className="container">
+        <div className="footer-wrapper">
+            <p>
+                Copyright 2022 <a href="#0" className="text-base">XYZ Bets</a> All Rights Reserved.
+            </p>
+            <ul className="footer-link">
+                <li>
+                    <a href="#0">
+                    Affiliate program
+                    </a>
+                </li>
+                <li>
+                    <a href="#0">
+                    Terms & conditions
+                    </a>
+                </li>
+                <li>
+                    <a href="#0">
+                    Bonus terms & conditions
+                    </a>
+                </li>
+            </ul>
+        </div>
+        </div>
+    </footer>
+  )
+}
+
+export default footer
